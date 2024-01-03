fa9c42becb3c386607341ff419a2141d6da3331b
parshimipopeli <philippe.lamorskipro@gmail.com>
2024-01-03 12:23:19 +0100

    Heritage class Monstre avec AbstractCombatant

--- src/main/java/fr/parshimipopeli/entity/Monstre.java
+++ src/main/java/fr/parshimipopeli/entity/Monstre.java
@@ -1,6 +1,6 @@
 package fr.parshimipopeli.entity;
 
-public class Monstre extends Personnage {
+public class Monstre extends AbstractCombatant {
     public Monstre(Integer pointDeVie, Integer degat, String nom) {
         super(nom, pointDeVie, degat);
     }
