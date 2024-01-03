c320d27b68e5d44eea38cd8734a401a53a935da6
parshimipopeli <philippe.lamorskipro@gmail.com>
2024-01-03 10:01:09 +0100

    création de la classe Personnage avec constructeur, getteur, setteur et methode toString

--- src/main/java/fr/parshimipopeli/entity/Personnage.java
+++ src/main/java/fr/parshimipopeli/entity/Personnage.java
@@ -1,2 +1,49 @@
-package fr.parshimipopeli.entity;public class Personnage {
+package fr.parshimipopeli.entity;
+
+import java.util.StringJoiner;
+
+public class Personnage {
+
+    private Integer pointDeVie;
+    private Integer degat;
+    private String nom;
+
+    public Personnage(Integer pointDeVie, Integer degat, String nom) {
+        this.pointDeVie = pointDeVie;
+        this.degat = degat;
+        this.nom = nom;
+    }
+
+    public Integer getPointDeVie() {
+        return pointDeVie;
+    }
+
+    public void setPointDeVie(Integer pointDeVie) {
+        this.pointDeVie = pointDeVie;
+    }
+
+    public Integer getDegat() {
+        return degat;
+    }
+
+    public void setDegat(Integer degat) {
+        this.degat = degat;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Personnage.class.getSimpleName() + "[", "]")
+                .add("pointDeVie=" + pointDeVie)
+                .add("degat=" + degat)
+                .add("nom='" + nom + "'")
+                .toString();
+    }
 }
